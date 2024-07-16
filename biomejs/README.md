10bda606db78273ad0440c90ad8a5ea67243b768
po4tion <po4tion0429@gmail.com>
2024-07-16 20:31:00 +0900

    init biome

---
+++ biomejs/README.md
@@ -0,0 +1,4 @@
+1. vite + react-ts
+2. install biome
+3. components > Button.tsx
+4. .vscode setting
---
+++ biomejs/src/components/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Button = (): React.JSX.Element => {
+  return <button type="button">버튼</button>;
+};
